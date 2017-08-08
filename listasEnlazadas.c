0110fabddbeef03a3c0ba1642c4f7ef96aeea153
Adrián Iglesias García <adrian_24-3-91@hotmail.com>
2017-08-08 10:33:04 +0200

    Add files via upload

--- listasEnlazadas.c
+++ listasEnlazadas.c
@@ -12,8 +12,34 @@ Lista = null;
 return Lista;
 }
 
+libro *agregarLibro(Libro *Lista, char *nombre){
+	libro *nuevoLibro, *aux;
+	nuevoLibro = malloc(sizeof(Libro));
+	nuevoLibro->nombre = nombre;
+	nuevoLibro->sig = null;
+	if (Lista == null){
+		Lista= nuevoLibro;
+	}else{
+	aux = Lista;
+	while(aux->sig != null){
+		aux = aux->sig;
+	
+		}
+			aux->sig = nuevoLibro;
+	}
+	return Lista;
+}
+
 int main(){
 	Libro *Lista = listaLibro(Lista);
-	printf("%p\n", Lista);
+	Lista = agregarLibro(Lista,"html");
+		Lista = agregarLibro(Lista,"css3");
+	
+while(Lista!=null){
+	printf("%s\n", Lista->nombre);
+	Lista = Lista->sig;
+}
+	
+	
 		return 0;
 }
